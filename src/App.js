b09ed56e960f5f0dfbda83431665ddc6d1fbdaf8
Tobiloba <omitomo.a@gotruck.io>
2021-05-08 00:16:09 +0100

    implement the library component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./styles/app.scss";
+import Player from "./components/Player";
+import Song from "./components/Songs";
+import data from "./util";
+import Library from './components/Library';
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <h1>Music player</h1>
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
+      <Library songs={songs} setCurrentSong={setCurrentSong}/>
     </div>
   );
 }
